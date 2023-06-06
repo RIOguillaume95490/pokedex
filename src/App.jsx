446596dc9efc9aea15002074420307cf9fae02d8
RIOguillaume95490 <guillaume.rio78@gmail.com>
2023-06-06 19:25:19 +0200

    add next and previous button react quest6

--- src/App.jsx
+++ src/App.jsx
@@ -7,21 +7,47 @@ import PokemonCard from './components/PokemonCard'
 
 const pokemonList = [
   {
-    name: "bulbasaur",
-    imgSrc:
-      "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png",
-  },
-  {
-    name: "mew",
-  },
-];
+      name: "bulbasaur",
+      imgSrc:
+        "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png",
+    },
+    {
+      name: "charmander",
+      imgSrc:
+        "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/4.png",
+    },
+    {
+      name: "squirtle",
+      imgSrc:
+        "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/7.png",
+    },
+    {
+      name: "pikachu",
+      imgSrc:
+        "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/25.png",
+    },
+    {
+      name: "mew",
+    },
+  ];
 
-function App() {
-  return (
-      <div>
-        <PokemonCard pokemon={pokemonList[0]}/>
-      </div>
-  );
-}
+const App =() => {
+  const[pokemonIndex, setpokemonIndex]=useState(0);
+  
+  const clickNext = () =>  setpokemonIndex(pokemonIndex +1);
+  const clickPrevious= () =>  setpokemonIndex(pokemonIndex -1);
+     
+      return ( 
+        <>
+          <PokemonCard pokemon={pokemonList[pokemonIndex]}/>
+          {pokemonIndex > 0 ? (
+            <input type="button" value="Précédent" onClick={clickPrevious}/>
+            ): null }
+          {pokemonIndex < pokemonList.length -1 ? (
+            <input type="button" value="suivant" onClick={clickNext}/>
+            ): null }
+        </>
+      )
+    }
 
 export default App
