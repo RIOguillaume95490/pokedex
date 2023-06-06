b8851ba0c4b0dbd616d83d37a7a196a47d65f39f
RIOguillaume95490 <guillaume.rio78@gmail.com>
2023-06-06 11:46:07 +0200

    add pokemon variable react quest 3

---
+++ src/components/PokemonCard.jsx
@@ -0,0 +1,27 @@
+import './PokemonCard.css'
+
+const pokemonList = [
+    {
+      name: "bulbasaur",
+      imgSrc:
+        "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png",
+    },
+    {
+      name: "mew",
+    },
+  ];
+
+function PokemonCard() {
+    let pokemon = pokemonList[1];
+    return (
+        <>
+        <figure className="card">
+            {pokemon.imgSrc != null ? <img className="card-img" src={pokemon.imgSrc} alt = {pokemon.name}/>
+                                    :<p>???</p>}
+            <figcaption>{pokemon.name}</figcaption>
+        </figure>
+        </>
+    );
+  }
+  
+  export default PokemonCard
