6303070f3f15a07f50c835fce91fe5a9f4ff2ed8
RIOguillaume95490 <guillaume.rio78@gmail.com>
2023-06-06 13:01:56 +0200

    add pokemonprops react quest 4

--- src/components/PokemonCard.jsx
+++ src/components/PokemonCard.jsx
@@ -1,18 +1,9 @@
 import './PokemonCard.css'
 
-const pokemonList = [
-    {
-      name: "bulbasaur",
-      imgSrc:
-        "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png",
-    },
-    {
-      name: "mew",
-    },
-  ];
 
-function PokemonCard() {
-    let pokemon = pokemonList[1];
+
+function PokemonCard({pokemon}) {
+  console.log(PokemonCard)
     return (
         <>
         <figure className="card">
